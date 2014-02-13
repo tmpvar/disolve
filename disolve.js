e905e70788d7edef71a048e3362e44c6525146c8
Elijah Insua <tmpvar@gmail.com>
2014-02-13 04:47:52 -0700

    add ExpressionGroup and ExpressionFunction + stringify

--- disolve.js
+++ disolve.js
@@ -3,10 +3,22 @@ var defined = function(a) {
   return typeof a !== 'undefined';
 };
 
+var string = function(a) {
+  return Object.prototype.toString.call(a) === '[object String]';
+}
+
 function Expression(left, operator, right) {
 
   if (!(this instanceof Expression)) {
     return new Expression(left, operator, right);
+  }
+
+  if (left instanceof Expression) {
+    left.parent = this;
+  }
+
+  if (right instanceof Expression) {
+    right.parent = this;
   }
 
   this.left = left;
@@ -14,7 +26,22 @@ function Expression(left, operator, right) {
   this.operator = new Operator(operator);
 }
 
+Expression.isExpression = function(a) {
+  return a && a.name && a.name === 'expression';
+}
+
+
+Expression.prototype.constructor = Expression;
+Expression.prototype.parent = null
 Expression.prototype.name = 'expression';
+
+Expression.prototype.toString = function(addParens) {
+  return [
+    this.left.toString(leftp),
+    this.operator.toString(),
+    this.right.toString(rightp),
+  ].join('');
+};
 
 Expression.prototype.evaluate = function(knowns) {
   var right = this.right;
@@ -56,6 +83,43 @@ ExpressionFor.prototype.evaluate = function(knowns) {
   return knowns[this.variable];
 };
 
+ExpressionFor.prototype.toString = function() {
+  return this.variable + ' = ' + this.expression.toString();
+};
+
+function ExpressionGroup(left, op, right) {
+  if (!(this instanceof ExpressionGroup)) {
+    return new ExpressionGroup(left, op, right);
+  }
+  Expression.call(this, left, op, right);
+}
+
+ExpressionGroup.prototype = Object.create(Expression.prototype);
+
+ExpressionGroup.prototype.constructor = ExpressionGroup;
+
+ExpressionGroup.prototype.toString = function() {
+  return '(' + Expression.prototype.toString.call(this) + ')';
+};
+
+
+function ExpressionFunction(fn, expression) {
+  if (!(this instanceof ExpressionFunction)) {
+    return new ExpressionFunction(fn, expression);
+  }
+
+  this.fn = fn;
+  this.expression = expression;
+}
+
+ExpressionFunction.prototype.evaluate = function() {
+
+};
+
+ExpressionFunction.prototype.toString = function() {
+  return this.fn + '(' + this.expression.toString() + ')';
+};
+
 function Operator(type) {
   if (!(this instanceof Operator)) {
     return new Operator(type);
@@ -82,8 +146,26 @@ Operator.prototype['/'] = function(a, b) {
   return a/b;
 };
 
+Operator.prototype['^'] = function(a, b) {
+  return Math.pow(a, b);
+};
+
+Operator.prototype.needsParens = function() {
+  return (this.type !== '*' && this.type !== '^');
+}
+
 Operator.prototype.perform = function(a, b) {
   return this[this.type](a, b);
+};
+
+Operator.prototype.toString = function() {
+  var type = ' ' + this.type + ' ';
+  if (this.type === '*') {
+    type = '';
+  } else if (this.type === '^') {
+    type = this.type;
+  }
+  return type;
 };
 
 
@@ -100,6 +182,8 @@ var disolve = function(val) {
 
 disolve.Expression = Expression;
 disolve.ExpressionFor = ExpressionFor;
+disolve.ExpressionFunction = ExpressionFunction;
+disolve.ExpressionGroup = ExpressionGroup;
 disolve.Operator = Operator;
 
 
--- test/test.js
+++ test/test.js
@@ -10,9 +10,52 @@ var eq = function(a, b) { if (a!==b) throw new Error(a + " !== " + b); }
 
 var Expression = disolve.Expression;
 var ExpressionFor = disolve.ExpressionFor;
+var ExpressionFunction = disolve.ExpressionFunction;
+var ExpressionGroup = disolve.ExpressionGroup;
 var Operator = disolve.Operator;
 
 describe('Expression', function() {
+  describe('debug', function() {
+    it('prints nicely "2x"', function() {
+      var e = Expression(2, '*', 'x');
+      eq(e.toString(), '2x');
+    });
+
+    it('prints nicely "2 + x / y"', function() {
+      var e = Expression(2, '+', Expression('x', '/', 'y'));
+      eq(e.toString(), '2 + x / y');
+    });
+
+    it('prints nicely "x / y + 2"', function() {
+      var e = Expression(Expression('x', '/', 'y'), '+', 2);
+      eq(e.toString(), 'x / y + 2');
+    });
+
+    it('prints nicely "2x / 2 / y"', function() {
+      var e = Expression(
+        Expression(2, '*', 'x'), '/', Expression(2, '/', 'y')
+      );
+
+      eq(e.toString(), '2x / 2 / y');
+    });
+
+    it('prints nicely "2x / 3y"', function() {
+      var e = Expression(
+        Expression(2, '*', 'x'), '/', Expression(3, '*', 'y')
+      );
+
+      eq(e.toString(), '2x / 3y');
+    });
+
+    it('prints nicely "2x / (3 + y)"', function() {
+      var e = Expression(
+        Expression(2, '*', 'x'), '/', ExpressionGroup(3, '+', 'y')
+      );
+
+      eq(e.toString(), '2x / (3 + y)');
+    });
+  });
+
   describe('basics', function() {
     it('performs basic aritmetic (add)', function() {
       var r = Expression('x', '+', 10).evaluate({ x: 10});
@@ -51,9 +94,73 @@ describe('Expression', function() {
       eq(e.evaluate(), 49);
     });
   });
+});
 
-  describe('shared knowns', function() {
-    it('bubbles knowns', function() {
+describe('ExpressionFunction', function() {
+  describe('debug', function() {
+    it('prints nicely "sqrt(a^2 + b^2 + c^2)"', function() {
+
+      var e = ExpressionFunction('sqrt',
+        Expression(
+          Expression('a', '^', 2),
+          '+',
+          Expression(
+            Expression('b', '^', 2),
+            '+',
+            Expression('c', '^', 2)
+          )
+        )
+      );
+
+      eq(e.toString(), 'sqrt(a^2 + b^2 + c^2)')
+    });
+  });
+});
+
+
+describe('ExpressionGroup', function() {
+  describe('debug', function() {
+    it('prints "sqrt((a^2 / 10) + c^2)"', function() {
+      var e = ExpressionFunction('sqrt',
+        Expression(
+          ExpressionGroup(
+            Expression('a', '^', 2),
+            '/',
+            10
+          ),
+          '+',
+          Expression('c', '^', 2)
+        )
+      );
+
+      eq(e.toString(), 'sqrt((a^2 / 10) + c^2)')
+
+    });
+  });
+});
+
+describe('ExpresionFor', function() {
+  describe('debug', function() {
+    it('prints nicely "y = mx + b"', function() {
+      var line = ExpressionFor('y',
+        Expression(Expression('m', '*', 'x'), '+', 'b')
+      );
+
+      eq(line.toString(), 'y = mx + b');
+    });
+
+    it('prints nicely "y = mx + b / 2"', function() {
+      var line = ExpressionFor('y',
+        Expression(Expression('m', '*', 'x'), '+', Expression('b', '/', 2))
+      );
+
+      eq(line.toString(), 'y = mx + b / 2');
+    });
+  });
+
+  describe('resolve unknown', function() {
+
+    it('resolves an unknown if all constraints are available', function() {
       var line = ExpressionFor('y', Expression(Expression('m', '*', 'x'), '+', 'b'));
 
       var knowns = {
@@ -66,5 +173,17 @@ describe('Expression', function() {
       eq(knowns.y, 10);
     });
 
+    it('reduces if not all params are known', function() {
+      var line = ExpressionFor('y',
+        Expression(
+          Expression('m', '*', 'x'), '+', 'b'
+        )
+      );
+
+      // var e =  line.evaluate({
+      //   m : 1,
+      //   b : 0
+      // };
+    });
   });
 });
