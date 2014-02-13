5c1ef72197f298a171b730c79d6b4ad669975a59
Elijah Insua <tmpvar@gmail.com>
2014-02-13 02:44:57 -0700

    basic expression/operator solving

--- disolve.js
+++ disolve.js
@@ -1,6 +1,106 @@
-var disolve = function() {
-  
+var microdom = require('microdom');
+var defined = function(a) {
+  return typeof a !== 'undefined';
+};
+
+function Expression(left, operator, right) {
+
+  if (!(this instanceof Expression)) {
+    return new Expression(left, operator, right);
+  }
+
+  this.left = left;
+  this.right = right;
+  this.operator = new Operator(operator);
 }
+
+Expression.prototype.name = 'expression';
+
+Expression.prototype.evaluate = function(knowns) {
+  var right = this.right;
+  var left = this.left;
+
+  if (knowns) {
+    if (defined(knowns[this.right])) {
+      right = knowns[this.right];
+    }
+
+    if (defined(knowns[this.left])) {
+      left = knowns[this.left];
+    }
+  }
+
+  if (right instanceof Expression) {
+    right = right.evaluate(knowns);
+  }
+
+  if (left instanceof Expression) {
+    left = left.evaluate(knowns);
+  }
+
+  return this.operator.perform(left, right)
+};
+
+function ExpressionFor(variable, expression) {
+
+  if (!(this instanceof ExpressionFor)) {
+    return new ExpressionFor(variable, expression);
+  }
+
+  this.variable = variable;
+  this.expression = expression;
+}
+
+ExpressionFor.prototype.evaluate = function(knowns) {
+  knowns[this.variable] = this.expression.evaluate(knowns)
+  return knowns[this.variable];
+};
+
+function Operator(type) {
+  if (!(this instanceof Operator)) {
+    return new Operator(type);
+  }
+
+  this.type = type;
+}
+
+Operator.prototype.name = 'operator';
+
+Operator.prototype['+'] = function(a, b) {
+  return a+b;
+};
+
+Operator.prototype['-'] = function(a, b) {
+  return a-b;
+};
+
+Operator.prototype['*'] = function(a, b) {
+  return a*b;
+};
+
+Operator.prototype['/'] = function(a, b) {
+  return a/b;
+};
+
+Operator.prototype.perform = function(a, b) {
+  return this[this.type](a, b);
+};
+
+
+
+function Value(val) {
+
+}
+
+
+var disolve = function(val) {
+  // TODO: parse incoming string into expression tree
+  return new Expression(val);
+};
+
+disolve.Expression = Expression;
+disolve.ExpressionFor = ExpressionFor;
+disolve.Operator = Operator;
 
 
 if (typeof module !== "undefined" && typeof module.exports == "object") {
--- test/test.js
+++ test/test.js
@@ -6,11 +6,65 @@ if (typeof require !== "undefined") {
 }
 
 var ok = function(a, msg) { if (!a) throw new Error(msg || "not ok"); };
+var eq = function(a, b) { if (a!==b) throw new Error(a + " !== " + b); }
 
-describe('disolve', function() {
-  describe('#', function() {
-    it('', function() {
-      
+var Expression = disolve.Expression;
+var ExpressionFor = disolve.ExpressionFor;
+var Operator = disolve.Operator;
+
+describe('Expression', function() {
+  describe('basics', function() {
+    it('performs basic aritmetic (add)', function() {
+      var r = Expression('x', '+', 10).evaluate({ x: 10});
+      eq(r, 20);
     });
+
+    it('performs basic aritmetic (subtract)', function() {
+      var r = Expression('x', '-', 1).evaluate({ x: 10});
+      eq(r, 9);
+    });
+
+    it('performs basic aritmetic (multiply)', function() {
+      var r = Expression('x', '*', 10).evaluate({ x: 10});
+      eq(r, 100);
+    });
+
+    it('performs basic aritmetic (divide)', function() {
+      var r = Expression('x', '/', 2).evaluate({ x: 10});
+      eq(r, 5);
+    });
+  });
+
+  describe('nested', function() {
+    it('properly nests expression (right)', function() {
+      var e = Expression(2, '*', Expression(5, '+', 2));
+      eq(e.evaluate(), 14);
+    });
+
+    it('properly nests expression (left)', function() {
+      var e = Expression(Expression(2, '+', 5), '*', 2);
+      eq(e.evaluate(), 14);
+    });
+
+    it('properly nests expression (both)', function() {
+      var e = Expression(Expression(2, '+', 5), '*', Expression(5, '+', 2));
+      eq(e.evaluate(), 49);
+    });
+  });
+
+  describe('shared knowns', function() {
+    it('bubbles knowns', function() {
+      var line = ExpressionFor('y', Expression(Expression('m', '*', 'x'), '+', 'b'));
+
+      var knowns = {
+        x : 10,
+        m : 1,
+        b : 0
+      };
+
+      eq(line.evaluate(knowns), 10);
+      eq(knowns.y, 10);
+    });
+
   });
 });
